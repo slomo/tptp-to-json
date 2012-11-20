6660b8b6236814897a272bd0e5d96cfe0ce0de5b
yves <steve.harrison@gmx.net>
2012-11-20 21:10:54 +0100

    prepend licences to main and remove some clutter

--- Main.hs
+++ Main.hs
@@ -1,22 +1,28 @@
+-- This program is free software. It comes without any warranty, to
+-- the extent permitted by applicable law. You can redistribute it
+-- and/or modify it under the terms of the Do What The Fuck You Want
+-- To Public License, Version 2, as published by Sam Hocevar. See
+-- http://sam.zoy.org/wtfpl/COPYING for more details.
+
 import Codec.TPTP
+
 import System.Cmd
 import System.IO
 import System.Environment
+
 import Data.Generics
 import Data.List
 
-
-import Debug.Trace
-
+-- just a simple wrapper for codec.TPTP, attention ugly code will follow
 
 main = do
   args <- getArgs
+  -- first parameter is used as input file name
   forms <- parseFile $ head args
   -- this strips comments and includes
   let formulas  = filter ( \x -> case x of
         (AFormula {}) -> True
         _ -> False ) forms
-  --let a =  parse "fof(ax, axiom, a(c) => ! [X] : b(X))."
   putStrLn $ "[" ++  ((intercalate ",\n") . (map jsonify)) formulas ++ "]"
 
 getString (AtomicWord s) = s
@@ -44,8 +50,6 @@ genTerm (Var v) = genVar v
 
 genVar (V s) = "{ \"type\" : \"variable\", \"name\": \"" ++ s ++ "\" }"
 
-
-
 genTermList list = "[ " ++ intercalate ", " ( map (genTerm . unwrapT) list) ++ "]"
 
 decodeQuantor q = "\"" ++ decodeQuantor1 q ++ "\""
@@ -61,8 +65,7 @@ decodeOp1 (:~&:) = "~&"
 decodeOp1 (:|:) = "|"
 decodeOp1 (:~|:) = "~|"
 
-
-jsonify a@(AFormula{}) = "{" ++
+jsonify a = "{" ++
             " \"name\" " ++ ": \"" ++ getName a ++ "\" ," ++
             " \"type\" " ++ ": \"" ++ getRole a ++ "\" ," ++
             " \"formula\" " ++ ":" ++ getFormula a ++ "}"
